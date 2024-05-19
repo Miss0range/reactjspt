45e5fed5da8a31cd2759e25053c1070a3cfacd5d
Miss0range <33188992+Miss0range@users.noreply.github.com>
2024-05-19 01:03:21 -0500

    5/19

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  const [counter, setCounter] = useState(15);//useState(initial value/state)
+  // let counter = 15;
+  const addValue = () => setCounter(counter+1);
+  const decreaseValue = () =>{
+    setCounter((counter)=> counter-1);
+  };
+
+  return (
+    <>
+      <h1>React bootcamp</h1>
+      <h2>Counter Value : {counter}</h2>
+      <button onClick={addValue}
+      >Add Value</button> {"     UwU    "}
+      <button onClick={decreaseValue}>Decrease Value</button>
+      <p>footer:{counter}</p>
+    </>
+  )
+}
+
+export default App
